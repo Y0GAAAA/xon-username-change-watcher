dcd38aff3693ce6c8fd9e2d0786990f3a27aa4cb
Y0GAAAA <35837405+Y0GAAAA@users.noreply.github.com>
2023-01-28 18:10:38 +0100

    use UserNotFoundException

--- database.py
+++ database.py
@@ -6,9 +6,12 @@ CREATE_TABLE = """CREATE TABLE IF NOT EXISTS players (
 ) WITHOUT ROWID ;"""
 
 GET_USERNAME = "SELECT username FROM players WHERE id = {};"
-
 UPDATE_USERNAME = "UPDATE players SET username = \"{}\" WHERE id = {};"
 INSERT_USERNAME = "INSERT INTO players (id, username) VALUES ({}, \"{}\");"
+
+class UserNotFoundException(Exception):
+    def __init__(self, _id) -> None:
+        super().__init__(f"user with id {_id} not found")
 
 class XonoticIdUsernameDatabase():
     def __init__(self, path) -> None:
@@ -18,7 +21,10 @@ class XonoticIdUsernameDatabase():
     def get_username(self, _id):
         query = GET_USERNAME.format(_id)
         cursor = self.connection.execute(query)
-        return str(cursor.fetchone()[0])
+        fetch = cursor.fetchone()
+        if not fetch:
+            raise UserNotFoundException(_id)
+        return str(fetch[0])
 
     def set_username(self, _id, username):
         query = UPDATE_USERNAME.format(username, _id)
